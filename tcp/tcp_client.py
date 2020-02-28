83c9572b1444fc4465670b83c6fb496d03cc1122
Sukhvir Singh <sukhvirsingh@Sukhvirs-MacBook-Pro.local>
2020-02-27 19:48:42 -0800

    added command line arguments

--- tcp/tcp_client.py
+++ tcp/tcp_client.py
@@ -1,4 +1,6 @@
 import socket
+import sys
+import time
 
 
 TCP_IP = '127.0.0.1'
@@ -6,18 +8,30 @@ TCP_PORT = 5000
 BUFFER_SIZE = 1024
 MESSAGE = "ping"
 
-def send(id=0):
+def send():
+    args = sys.argv
+    id = args[1]
+    delay = int(args[2])
+    numOfMessages = int(args[3])
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
-    s.send(f"{id}:{MESSAGE}".encode())
-    data = s.recv(BUFFER_SIZE)
-    s.close()
-    print("received data:", data.decode())
 
+    for i in range(numOfMessages):
+        print('Sending data: ping')
+        s.send(f"{id}:{MESSAGE}".encode())
+        data = s.recv(BUFFER_SIZE)
+        print(f'Received data: {data.decode()}')
+        # no need for delay if sending the last message
+        if (i+1) != numOfMessages:
+            time.sleep(delay)
+    
+    s.close()
 
 def get_client_id():
     id = input("Enter client id:")
     return id
 
 
-send(get_client_id())
+# send(get_client_id())
+send()
